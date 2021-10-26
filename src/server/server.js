de4d82137c773314e6a7a896714cb700549e8f82
aymen isfiaya <aymen.isfiaya@gmail.com>
2021-10-26 18:14:38 +0200

    fix port server

--- src/server/server.js
+++ src/server/server.js
@@ -34,7 +34,7 @@ app.get('/key', async (req, res) => {
 
 
 // Setup Server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
