c0f8495f8b334d3cd5fe2274bef8a07ae52dad06
aymen isfiaya <aymen.isfiaya@gmail.com>
2021-10-27 10:47:23 +0200

    remove: Comment

--- src/server/server.js
+++ src/server/server.js
@@ -7,22 +7,15 @@ const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 app.use(cors());
-
 /* Middleware*/
 // Initialize the main project folder
 app.use(express.static('dist'));
 // Routes
 //GET Route
-// app.get('/', async (req, res) => {
-//   res.sendFile(path.resolve('src/client/views/index.html'))
-// })
-
 app.get('/', function (req, res) {
   res.sendFile('dist/index.html');
 })
-
 app.get('/key', async (req, res) => {
   res.send({
     geoUserName: process.env.USERNAME_GEONAMES,
@@ -30,15 +23,9 @@ app.get('/key', async (req, res) => {
     apiKeyPixaBay: process.env.API_KEY_PIXABAY
   })
 })
-app.get('/test', async (req, res) => {
-  res.json({ msg: 'pass!' })
-})
-
-
 // Setup Server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
 module.exports = app
